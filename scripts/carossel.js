6f28668fc21795dfa0a6c6e47d3102c6d61a6d47
19aninha <fana47368@gmail.com>
2024-10-10 16:02:32 -0300

    projeto atualização

--- scripts/carossel.js
+++ scripts/carossel.js
@@ -3,7 +3,7 @@ let index = 0;
 function moveSlide(step) {
   const slides = document.querySelectorAll('.carousel-images img');
   index = (index + step + slides.length) % slides.length; // Cálculo do índice
-  const offset = -index * 800; // Ajuste para o tamanho da imagem
+  const offset = -index * 500; // Ajuste para o tamanho da imagem
   document.querySelector('.carousel-images').style.transform = `translateX(${offset}px)`;
 }
 
